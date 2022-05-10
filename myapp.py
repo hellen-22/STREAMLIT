1d0ead72496872e71d1089306f8cf20645993b7b
Hellen Wainaina <58620060+hellen-22@users.noreply.github.com>
2022-05-10 23:52:40 +0300

    streamlit secrets and file uploader

--- myapp.py
+++ myapp.py
@@ -88,3 +88,13 @@ recommend_button = st.button('SUBMIT')
 
 #st.latex-for mathematical formulas
 
+uploaded_file = st.file_uploader("Choose a csv file")
+
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
+  st.subheader('DataFrame')
+  st.write(df)
+  st.subheader('Descriptive Statistics')
+  st.write(df.describe())
+else:
+  st.info('☝️ Upload a CSV file')
